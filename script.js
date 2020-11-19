c16832997ad25c6f40756e02b76622816925fc36
b3-bogusz <b3.bogusz@gmail.com>
2020-11-19 22:24:29 +0100

    starting condition

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 'use strict';
 
+// Selecting elements //
+const scoreP0 = document.getElementById('score--0');
+const scoreP1 = document.getElementById('score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
+// Starting conditions //
+scoreP0.textContent = 0;
+scoreP1.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 
 
